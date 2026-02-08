f1faebbe6d319df593c840007b5408d98800975a
Wojciech Kania <wojtek@kania.sh>
2026-02-08 20:39:13 +0100

    [nvim] catpuccin theme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,30 @@
+-- mini.nvim
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    -- Uncomment next line to use 'stable' branch
+    -- '--branch', 'stable',
+    'https://github.com/nvim-mini/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require('mini.deps').setup()
+-- mini.nvim
+--
+-- mini deps
+local add = MiniDeps.add
+add({ source = "catppuccin/nvim", name = "catppuccin" })
+-- mini deps
+
+-- nvim config
 vim.o.number = true
 vim.o.relativenumber = true
+
+vim.cmd.colorscheme("catppuccin-macchiato")
+-- nvim config
