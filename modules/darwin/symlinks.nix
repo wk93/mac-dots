6a2cfb6f060b2107bc9cfcb4c2bad5c24b2a7953
Wojciech Kania <wojtek@kania.sh>
2026-02-08 19:26:55 +0100

    add ghostty config

--- modules/darwin/symlinks.nix
+++ modules/darwin/symlinks.nix
@@ -2,5 +2,8 @@
 {
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/nvim";
+
+  home.file.".config/ghostty".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/ghostty";
 }
 
