272deb1ba1027299cb1da8462cb9ef4e44ff88f0
Wojciech Kania <wojtek@kania.sh>
2026-02-09 14:36:34 +0100

    [nvim] snacks picker

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,18 +13,38 @@ if not vim.loop.fs_stat(mini_path) then
   vim.cmd('packadd mini.nvim | helptags ALL')
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
-
-require('mini.deps').setup()
 -- mini.nvim
---
+
 -- mini deps
+require('mini.deps').setup()
 local add = MiniDeps.add
 add({ source = "catppuccin/nvim", name = "catppuccin" })
+
+add({ source = "folke/snacks.nvim", name = "snacks" })
 -- mini deps
 
 -- nvim config
+vim.g.mapleader = ' '
+
 vim.o.number = true
 vim.o.relativenumber = true
 
 vim.cmd.colorscheme("catppuccin-macchiato")
 -- nvim config
+
+-- keymaps
+local nmap = function(lhs, rhs, desc)
+  -- See `:h vim.keymap.set()`
+  vim.keymap.set('n', lhs, rhs, { desc = desc })
+end
+
+local nmap_leader = function(suffix, rhs, desc)
+  vim.keymap.set('n', '<Leader>' .. suffix, rhs, { desc = desc })
+end
+
+---- snacks picker
+nmap_leader('fb', function() Snacks.picker.buffers() end, 'Buffers')
+nmap_leader('ff', function() Snacks.picker.git_files() end, 'Files')
+nmap_leader('fg', function() Snacks.picker.grep() end, 'Grep live')
+---- snacks picker
+-- keymaps
