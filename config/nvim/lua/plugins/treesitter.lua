4e1f3f8fee064bce124afb32383aeef6e5cea851
Wojciech Kania <wojtek@kania.sh>
2026-02-16 13:54:33 +0100

    [nvim] disable treesitter on large files

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
-vim.pack.add({{
+vim.pack.add({ {
 	src = "https://github.com/nvim-treesitter/nvim-treesitter",
-	version = "main"
-}})
+	version = "main",
+} })
 
-require("nvim-treesitter").setup {
+require("nvim-treesitter").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -39,11 +39,29 @@ require("nvim-treesitter").setup {
 			})
 
 			vim.api.nvim_create_autocmd("FileType", {
-				callback = function()
-					pcall(vim.treesitter.start)
+				callback = function(args)
+					local function should_disable_for_buf(buf)
+						local max_filesize = 100 * 1024 -- 100 KiB
+						local name = vim.api.nvim_buf_get_name(buf)
+						if name == "" then
+							return false
+						end
+
+						local ok, stats = pcall(vim.loop.fs_stat, name)
+						return ok and stats and stats.size > max_filesize
+					end
+
+					if vim.bo[args.buf].buftype ~= "" then
+						return
+					end
+					if should_disable_for_buf(args.buf) then
+						return
+					end
+
+					pcall(vim.treesitter.start, args.buf)
 				end,
 			})
 			vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
-	}
-}
+	},
+})
