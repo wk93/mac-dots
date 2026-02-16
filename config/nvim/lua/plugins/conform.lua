bec89d5d1a24e5dea2e85a264c456084a6126e55
Wojciech Kania <wojtek@kania.sh>
2026-02-16 13:48:54 +0100

    [nvim] conform

---
+++ config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+vim.pack.add({
+	"https://github.com/stevearc/conform.nvim",
+})
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "alejandra" },
+
+		javascript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		json = { "prettierd" },
+		yaml = { "prettierd" },
+		html = { "prettierd" },
+		css = { "prettierd" },
+		markdown = { "prettierd" },
+	},
+	default_format_opts = {
+		lsp_format = "fallback",
+	},
+	format_on_save = {
+		lsp_format = "fallback",
+		timeout_ms = 500,
+	},
+	notify_on_error = true,
+})
