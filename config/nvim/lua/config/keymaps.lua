6670834bd105487f29e68a12abba6b8a6d452acd
Wojciech Kania <wojtek@kania.sh>
2026-02-16 11:08:44 +0100

    [nvim] new config for 0.12

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.swapfile = false
+
+vim.g.mapleader = " "
