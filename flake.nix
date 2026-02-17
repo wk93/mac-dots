a0eb80ebcff4e242bf2aef7dcba61c51ba05808c
Wojciech Kania <wojtek@kania.sh>
2026-02-17 16:03:50 +0100

    [sys] final remove nixos

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Starter Configuration for MacOS and NixOS";
+  description = "Starter Configuration for MacOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
